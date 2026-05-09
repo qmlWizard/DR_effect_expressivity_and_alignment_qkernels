4d8029f0a8ba3f108360f5ba863ba0dc254ac026
qmlWizard <digvijayajarekar.0@gmail.com>
2026-05-09 08:50:13 +0530

    updated results

--- run.sh
+++
@@ -1,5 +0,0 @@
-python3 run_kta_experiments.py --dataset torus
-python3 run_kta_experiments.py --dataset mobius
-python3 run_kta_experiments.py --dataset lorenz
-python3 run_kta_experiments.py --dataset two_moons
-python3 run_kta_experiments.py --dataset sierpinski/
